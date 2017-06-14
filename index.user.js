9cf38d9bb919fe71c72661ff7b4a9789877f252e
seliver <alexeyseliverstov.dev@gmail.com>
2017-06-14 12:09:50 +0100

    Create index.user.js

--- index.user.js
+++ index.user.js
@@ -4,7 +4,9 @@
 // @description   Jira links in a new tab
 // @namespace     https://github.com/seliver/
 // @match         https://*.atlassian.net/secure/RapidBoard.jspa*
+// @match         https://*.atlassian.com/secure/RapidBoard.jspa*
 // @match         https://*.atlassian.com/browse/*
+// @match         https://*.atlassian.net/browse/*
 // @author        Alexey Seliverstov
 // @updateURL     https://raw.githubusercontent.com/seliver/jira-links-newtab/master/index.user.js
 // @downloadURL   https://raw.githubusercontent.com/seliver/jira-links-newtab/master/index.user.js
